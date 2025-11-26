f0831785e677abf823d4ff6c4ab2ada1feadbead
Oscar-am26 <oscar.alegmar@gmail.com>
2025-11-25 19:57:04 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+
+const CACHE = 'bee-pwa-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './dojo.html',
+  './celebration.html',
+  './manifest.json',
+  './data/words.json'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
